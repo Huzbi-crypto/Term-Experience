afc472c4676f2478cb7028b9212c7be6fe2c29be
Huzbi <76440095+Huzbi-crypto@users.noreply.github.com>
2023-09-30 15:01:18 +0500

    📂 Added source files and assets for the project.

---
+++ js/commands.js
@@ -0,0 +1,79 @@
+var mal = "https://myanimelist.net/profile/Huzbi";
+var twitter = "https://twitter.com/HuzbiC";
+var discord = "https://discordapp.com/users/653883645546856448";
+var instagram = "https://www.instagram.com/Huzbinaru/";
+var github = "https://github.com/Huzbi-crypto";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:huzbi@protonmail.com";
+var hentai = "https://www.youtube.com/watch?v=Jrg9KxGNeJY";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Yo, it's Huzbi!👋</span>",
+  "<br>",
+  "<li>🔭 A weeb learning to make websites</li>",
+  "<li>🌱 My fields of interests are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - ML and AI
+        - Anime, Manga, and Books</pre>`,
+  `<li><pre class="whitespace-pre-wrap">⌛ Time is limited and so much is left. I'll have to work harder to 
+   read more manga and do more projects!
+   I love learning new stuff and using it to improve my skills.
+   You can browse around my website to find out more stuff and probably 
+   some hidden stuff for weebs 😏 (coming soon)...
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view other stuff I've created.
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=huzbi@protonmail.com">huzbi@protonmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'mal        <a href="' +
+    mal +
+    '" target="blank">myanimelist.com/Huzbi' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter.com/HuzbiC' +
+    "</a>",
+  'discord       <a href="' +
+    discord +
+    '" target="blank">discord.com/huzbi' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram/Huzbinaru' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/Huzbi-crypto' +
+    "</a>",
+  
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me an email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.
+  <span class="command">weeb</span>
+   <span class="command">↳</span>&nbsp;Find weeb commands. 😉</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to Exclusive Terminal Experience! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
